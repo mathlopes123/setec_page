bfda61fed9fd09f71ce2ebe7905d3bbb8aef28d9
mathlopes123 <142631641+mathlopes123@users.noreply.github.com>
2025-06-16 11:38:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@
       });
 
 
-const script_do_google = 'https://script.google.com/macros/s/AKfycbyRGhif5NaKkEFyHA5CTJCQQ5zQ01gAC-OpOM-vkWraRJyyaNAlKpmw19_Q2eIV2Rsg/exec'
+const script_do_google = 'https://script.google.com/macros/s/AKfycbyRGhif5NaKkEFyHA5CTJCQQ5zQ01gAC-OpOM-vkWraRJyyaNAlKpmw19_Q2eIV2Rsg/exec';
 const dados_do_formulario = document.forms['formulario-contato'];
 
 dados_do_formulario.addEventListener('submit', function (e) {
